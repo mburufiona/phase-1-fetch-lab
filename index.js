28b7f8f80d788862071a563743c8f1a0f7fae6e6
fiona <fiona.mburu@student.moringaschool.com>
2024-07-25 17:03:03 +0300

    adds fetch

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((res) => res.json())
+  .then((data) => {
+    renderBooks(data);
+
+  })
   
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(booksData) {
+  const titles = booksData.map((book) => book.name);
+  console.log("List of Book Titles:", titles);
 }
-
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
